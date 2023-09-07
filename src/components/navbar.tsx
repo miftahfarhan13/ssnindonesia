90dce143aea9d58c7658e56235e9f426ac14725e
Fachril Zulfidar <fachril21@gmail.com>
2023-09-07 20:18:26 +0700

    navbar & header

---
+++ src/components/navbar.tsx
@@ -0,0 +1,37 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0">
+      <div className="container px-1">
+        <a className="navbar-brand fw-bolder fs-5 ms-lg-0 " href="https://www.creative-tim.com/astro">Samudra Sukses Nusantara</a>
+        <button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon mt-2">
+            <span className="navbar-toggler-bar bar1"></span>
+            <span className="navbar-toggler-bar bar2"></span>
+            <span className="navbar-toggler-bar bar3"></span>
+          </span>
+        </button>
+        <div className="collapse navbar-collapse" id="navigation">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="/astro-ecommerce/">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="/astro-ecommerce/">
+                Product
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
